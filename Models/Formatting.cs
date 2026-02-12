b5dc355cdc4aa01040d8f3385b9746e572e6297f
gaffelsteinkh <GaffelsteinKH@gmail.com>
2026-02-12 09:46:15 +0100

    Startup. csv reader

---
+++ Models/Formatting.cs
@@ -0,0 +1,17 @@
+namespace Formatting
+{
+        
+    public class Format
+    {
+        public string? model;
+        public int year;
+        public double price;
+        public string? transmission;
+        public double mileage;
+        public string? fuelType;
+        public double tax;
+        public double mpg;
+        public double engineSize;
+
+    }
+}
